f138ebd7f22ff7bf4b09f554eb31faf102475368
Ahmad Ali <spyali463@gmail.com>
2024-12-11 21:51:21 +0500

    for loop

---
+++ foorloop.py
@@ -0,0 +1,63 @@
+# list = ["Ahmad ","Ali","Saad","Haider"]
+# for char in list:
+#     print(char)
+
+
+
+
+
+# str = "Ahmad Ali"
+# for char in str:
+#         if (char == "l"):
+#             print("found")
+#             break
+#         print(char)
+# else:
+#           print("error")
+
+
+# tup= (1,4,9,16,25,49,81,100,49)
+# x = 49
+# idx=0
+# for el in tup:
+#   if(el== 49 ):
+#     print("number found at idx",idx)
+#   idx += 1
+
+
+
+
+# for i in range(10):
+#     print(i)
+# for i in range(1,20,3):
+#       print(i)
+   
+
+# for i in range(100,0,-1):
+#     print(i)
+
+
+
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact*=i
+#     print(f"the factorial {n}is{fact}")
+
+# print("Even numbers from 1 to", n, "are:")
+# for i in range(1, n + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+
+
+n = int(input("Enter a number: "))
+
+for i in range(n, 0, -1):
+    print(i)
+
+
+
+
+
+
